3cb3573eb7d838e9cb9a37b7e7d11c665c00140c
fmsthird <floranteIII.miranda.sugian@gmail.com>
2022-08-25 02:13:24 +0800

    fix padding

--- src/components/Header/header.styles.js
+++ src/components/Header/header.styles.js
@@ -4,10 +4,12 @@ export const HeaderMain = styled.header`
   border-bottom: 1px solid #eaecf0;
 
   nav {
-    padding: 12px 0;
+    padding-top: 12px;
+    padding-bottom: 12px;
 
     @media (min-width: 768px) {
-      padding: 16px 0;
+      padding-top: 16px;
+      padding-bottom: 16px;
     }
   }
 
@@ -15,7 +17,7 @@ export const HeaderMain = styled.header`
     border: 1px solid #d0d5dd;
     box-shadow: 0 1px 2px rgb(16 24 40 / 5%);
     border-radius: 8px;
-    padding: 10px 18px;
+    padding: 8px 14px;
     font-family: "Raleway", sans-serif;
     font-style: normal;
     font-weight: 500;
@@ -23,6 +25,10 @@ export const HeaderMain = styled.header`
     line-height: 24px;
     color: #344054;
     user-select: none;
+
+    @media (min-width: 768px) {
+      padding: 10px 18px;
+    }
 
     svg {
       margin-top: -4px;
