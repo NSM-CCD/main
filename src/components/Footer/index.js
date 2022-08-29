5332d0beb08befb833bd07140e187672fe679dc8
fmsthird <floranteIII.miranda.sugian@gmail.com>
2022-08-30 00:37:33 +0800

    refactor footer

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -1,22 +1,65 @@
 import React from "react"
-import Footer2 from "./components/Footer2"
+import { Link } from "gatsby"
+import footerLogo from "../../images/footer-logo.svg"
+import {
+  Content,
+  Copyright,
+  FooterBottom,
+  FooterLinks,
+  FooterMain,
+  Menus,
+  PrivacyTerms,
+} from "./footer.styles"
 
-const Footer = () => <Footer2 topperProps={footer2Data.topperProps} />
+const Footer = () => (
+  <FooterMain>
+    <div className="container p-0">
+      <Content>
+        <FooterLinks>
+          <Link to="/" className="footer-logo">
+            <img src={footerLogo} alt="Footer logo" />
+          </Link>
+
+          <Menus>
+            <div className="footer-menu">
+              <p className="menu-title">Partners</p>
+              <a href="#">Classiccars.com</a>
+              <a href="#">Hemmings</a>
+              <a href="#">americanmusclecarz.com</a>
+              <a href="#">classicandcollectorcars.com</a>
+              <a href="#">Auto Trader - Classics</a>
+              <a href="#">eBay Motors</a>
+            </div>
+
+            <div className="footer-menu">
+              <p className="menu-title">Helpful Links</p>
+              <a href="#">Get a Quote</a>
+              <a href="#">ACI Blog</a>
+              <a href="#">Insurance Policies</a>
+            </div>
+          </Menus>
+        </FooterLinks>
+        <hr />
+        <FooterBottom>
+          <Copyright>Classic Car Value 2023</Copyright>
+          <PrivacyTerms>
+            <a
+              href="https://americancollectors.com/privacy-policy/"
+              className="privacy-policy"
+            >
+              Privacy Policy
+            </a>
+            <a
+              href="https://americancollectors.com/terms-of-use/"
+              className="terms-conditions"
+            >
+              Terms & Conditions
+            </a>
+          </PrivacyTerms>
+        </FooterBottom>
+      </Content>
+    </div>
+  </FooterMain>
+)
 
 export default Footer
-
-const linkColumnData = {
-  classiccarsCom: "Classiccars.com",
-}
-
-const linksData = {
-  linkColumnProps: linkColumnData,
-}
-
-const topperData = {
-  linksProps: linksData,
-}
-
-const footer2Data = {
-  topperProps: topperData,
-}
