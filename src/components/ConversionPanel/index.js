1da212cdb0738dc58d153566bd3b14f755998d57
fmsthird <floranteIII.miranda.sugian@gmail.com>
2022-08-30 00:38:01 +0800

    initial conversion panel

---
+++ src/components/ConversionPanel/index.js
@@ -0,0 +1,12 @@
+import React from "react"
+import styled from "styled-components"
+
+const ConversionPanel = () => {
+  return <div>conversionPanel</div>
+}
+
+export default ConversionPanel
+
+export const ConversionPanelSection = styled.section`
+  padding: 40px 24px;
+`
--- src/pages/index.js
+++ src/pages/index.js
@@ -4,12 +4,14 @@ import Layout from "../components/layout"
 import Seo from "../components/seo"
 import Hero from "../components/Hero"
 import OffsetGrid from "../components/OffsetGrid"
+import ConversionPanel from "../components/ConversionPanel"
 
 const IndexPage = () => (
   <Layout>
     <Seo title="Home" />
     <Hero />
     <OffsetGrid />
+    <ConversionPanel />
   </Layout>
 )
 
