bad53197b075461af684b81d9f5399fca63d9fc2
fmsthird <floranteIII.miranda.sugian@gmail.com>
2022-08-29 22:34:25 +0800

    refactor offset grid

--- src/components/OffsetGrid/index.js
+++ src/components/OffsetGrid/index.js
@@ -1,58 +1,67 @@
 import React from "react"
-import FeatureGrid2 from "./components/FeatureGrid2"
+import {
+  Content,
+  GridBlock,
+  HeadingBlock,
+  OffsetGridSection,
+} from "./offsetgrid.styles"
+import checkIcon from "../../images/icons/check-icon.svg"
 
 const OffsetGrid = () => (
-  <FeatureGrid2
-    headingKicker="Top-tier Analysis"
-    spanText1="Not just another "
-    spanText2="valuation tool"
-    spanText3="."
-    howMuchIsYourCla="Information is power. American Collectors Insurance has partnered with experienced valuation experts like NADA and VMR Auto Guides to create a better classic car valuation tool."
-    containerProps={featureGrid2Data.containerProps}
-  />
+  <OffsetGridSection>
+    <div className="container px-0">
+      <Content>
+        <HeadingBlock>
+          <span className="kicker">Top-tier Analysis</span>
+          <h2 className="heading">
+            Not just another <span>valuation tool</span>.
+          </h2>
+          <p className="description">
+            Information is power. American Collectors Insurance has partnered
+            with experienced valuation experts like NADA and VMR Auto Guides to
+            create a better classic car valuation tool.
+          </p>
+        </HeadingBlock>
+        <GridBlock>
+          <div className="grid-item">
+            <img className="check-icon" src={checkIcon} alt="Check icon" />
+            <p className="grid-title">Market Value</p>
+            <p className="grid-description">
+              Determined analyzing supply and demand, using historical sale
+              prices and/or current asking prices on available inventory.
+            </p>
+          </div>
+
+          <div className="grid-item">
+            <img className="check-icon" src={checkIcon} alt="Check icon" />
+            <p className="grid-title">Compare Sources</p>
+            <p className="grid-description">
+              Comparing results from multiple valuation sources will help
+              develop a more accurate value estimate for your collector car!
+            </p>
+          </div>
+
+          <div className="grid-item">
+            <img className="check-icon" src={checkIcon} alt="Check icon" />
+            <p className="grid-title">Accurate Valuations</p>
+            <p className="grid-description">
+              No single valuation source can precisely determine a classic car’s
+              value.
+            </p>
+          </div>
+
+          <div className="grid-item">
+            <img className="check-icon" src={checkIcon} alt="Check icon" />
+            <p className="grid-title">Sales Background</p>
+            <p className="grid-description">
+              70% of classic / antique cars are sold through private party
+              sales, 20% through auctions and 10% through dealerships.
+            </p>
+          </div>
+        </GridBlock>
+      </Content>
+    </div>
+  </OffsetGridSection>
 )
 
 export default OffsetGrid
-
-const feature1Data = {
-  marketValue: "Market Value",
-  determinedAnalyzing:
-    "Determined analyzing supply and demand, using historical sale prices and/or current asking prices on available inventory.",
-}
-
-const feature2Data = {
-  marketValue: "Compare Sources",
-  determinedAnalyzing:
-    "Comparing results from multiple valuation sources will help develop a more accurate value estimate for your collector car!",
-}
-
-const frame11Data = {
-  feature1Props: feature1Data,
-  feature2Props: feature2Data,
-}
-
-const feature3Data = {
-  marketValue: "Accurate Valuations",
-  determinedAnalyzing:
-    "No single valuation source can precisely determine a classic car’s value.",
-}
-
-const feature4Data = {
-  marketValue: "Sales Background",
-  determinedAnalyzing:
-    "70% of classic / antique cars are sold through private party sales, 20% through auctions and 10% through dealerships.",
-}
-
-const frame12Data = {
-  feature1Props: feature3Data,
-  feature2Props: feature4Data,
-}
-
-const containerData = {
-  frame11Props: frame11Data,
-  frame12Props: frame12Data,
-}
-
-const featureGrid2Data = {
-  containerProps: containerData,
-}
---
+++ src/components/OffsetGrid/offsetgrid.styles.js
@@ -0,0 +1,140 @@
+import styled from "styled-components"
+
+export const OffsetGridSection = styled.section`
+  background-color: #fff;
+
+  .container {
+    @media (min-width: 1366px) {
+      max-width: 1170px;
+      margin: 0 auto;
+    }
+  }
+`
+
+export const Content = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  gap: 48px;
+  padding: 40px 24px;
+
+  @media (min-width: 768px) {
+    padding: 80px 0;
+  }
+
+  @media (min-width: 992px) {
+    padding: 96px 0;
+    flex-direction: row;
+  }
+`
+
+export const HeadingBlock = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 24px;
+
+  @media (min-width: 992px) {
+    max-width: 500px;
+  }
+
+  span.kicker {
+    font-family: "Raleway", sans-serif;
+    font-style: normal;
+    font-weight: 700;
+    font-size: 16px;
+    line-height: 24px;
+    color: #5e8ab4;
+  }
+
+  .heading {
+    font-family: "Raleway", sans-serif;
+    font-style: normal;
+    font-weight: 700;
+    font-size: 24px;
+    line-height: 32px;
+    color: #101828;
+    margin: 0;
+
+    span {
+      color: #5e8ab4;
+    }
+
+    @media (min-width: 768px) {
+      font-size: 36px;
+      line-height: 44px;
+      letter-spacing: -0.02em;
+    }
+
+    @media (min-width: 992px) {
+      font-size: 48px;
+      line-height: 60px;
+    }
+
+    .description {
+      font-family: "Raleway", sans-serif;
+      font-style: normal;
+      font-weight: 400;
+      font-size: 20px;
+      line-height: 30px;
+      color: #475467;
+      margin: 0;
+    }
+  }
+`
+
+export const GridBlock = styled.div`
+  display: flex;
+  flex-direction: column;
+  flex-wrap: wrap;
+  gap: 32px;
+
+  @media (min-width: 768px) {
+    flex-direction: row;
+    gap: 30px;
+  }
+
+  .grid-item {
+    display: flex;
+    flex-direction: column;
+    gap: 12px;
+
+    @media (min-width: 768px) {
+      flex: 0 50%;
+      max-width: 345px;
+    }
+
+    @media (min-width: 992px) {
+      max-width: 190px;
+    }
+
+    @media (min-width: 1200px) {
+      max-width: 270px;
+    }
+
+    .check-icon {
+      width: 100%;
+      max-width: 32px;
+      margin-bottom: 20px;
+    }
+
+    .grid-title {
+      font-family: "Raleway", sans-serif;
+      font-style: normal;
+      font-weight: 600;
+      font-size: 18px;
+      line-height: 28px;
+      color: #101828;
+      margin: 0;
+    }
+
+    .grid-description {
+      font-family: "Raleway", sans-serif;
+      font-style: normal;
+      font-weight: 400;
+      font-size: 18px;
+      line-height: 28px;
+      color: #475467;
+      margin: 0;
+    }
+  }
+`
