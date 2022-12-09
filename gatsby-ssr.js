ba0f91338fe071dabe169a0c2c567b39114cf11d
fmsthird <floranteIII.miranda.sugian@gmail.com>
2022-12-10 01:22:38 +0800

    add gtag script

--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -6,6 +6,8 @@ export const onRenderBody = ({ setHeadComponents, setPostBodyComponents }) => {
       src="https://cdn-us.clickdimensions.com/web/v10/cdform.jquery.js"
       type="text/javascript"
     />,
+    <script type="text/javascript" dangerouslySetInnerHTML={{
+      __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src='https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);})(window,document,'script','dataLayer','GTM-5ZCKKD');`}} />,
     <script
       type="text/javascript"
       dangerouslySetInnerHTML={{
@@ -31,6 +33,7 @@ export const onRenderBody = ({ setHeadComponents, setPostBodyComponents }) => {
   ])
 
   setPostBodyComponents([
+    <noscript dangerouslySetInnerHTML={{ __html: '<iframe src="https://www.googletagmanager.com/ns.html?id=GTM-5ZCKKD" height="0" width="0" style="display:none;visibility:hidden" />'}} />,
     <script
       type="text/javascript"
       dangerouslySetInnerHTML={{
