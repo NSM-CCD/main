a34514387eccd2f979578970ee7d46c1246cef0a
fmsthird <floranteIII.miranda.sugian@gmail.com>
2023-01-20 03:51:13 +0800

    fix build error

--- src/utils/getVisitorKey.js
+++ src/utils/getVisitorKey.js
@@ -2,4 +2,4 @@ import cookie from "cookie"
 
 const { cuvid } = cookie.parse(document.cookie)
 
-export const getVisitorKey = () => cuvid
+export const getVisitorKey = () => typeof window !== undefined && cuvid
