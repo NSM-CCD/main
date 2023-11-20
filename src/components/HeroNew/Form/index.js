c951b36b862e838abee55dc7538cbf5f38377c88
NSM-CCD <150178814+NSM-CCD@users.noreply.github.com>
2023-11-20 14:20:46 -0500

    Update index.js - removed canonicalUrl variable

--- src/components/HeroNew/Form/index.js
+++ src/components/HeroNew/Form/index.js
@@ -61,7 +61,6 @@ const HeroForm = ({
       <div className="form-container">
         <h2 className="form-title">Calculate the value of your classic car</h2>
         <p className="form-subtitle">Enter the details of your vehicle</p>
-        <p className="form-subtitle">{canonicalUrl}</p>
         <div className="form">
           <div className="selector">
             <button
